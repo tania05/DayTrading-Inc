94ddf078b6cccfe83b5d8e275554cde4b591d92f
Tania Akter <tania9309@gmail.com>
2018-01-17 00:43:28 -0800

    mapping users to one command, grouped by userID

--- load-generator/main.go
+++ load-generator/main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 import "io/ioutil"
 import "strings"
+import "strconv"
 
 //type AddCommand struct {
 //  userId string
@@ -24,7 +25,7 @@ type Command interface {
   name() string
 }
 
-func (command *BuyCommand) name() string {
+func (command BuyCommand) name() string {
   return fmt.Sprintf("userid %s stocksymbol %s amount %d", command.userId, command.stockSymbol, command.amount)
 }
 
@@ -36,21 +37,31 @@ func main() {
   }
 
   lines := strings.Split(string(b), "\n")
-
-  userMap := make(map[string][]string)
-  x := 0
+  //// "kk", []
+  userMap := make(map[string][]Command)
   for _, line := range lines {
-    x += 2
+    line = strings.Trim(line, "\n\r \t")
     parts := strings.Split(line, ",")
     if len(parts) < 2 {
       continue
     }
+    
+    userId := parts[1]
 
-    switch commandType := strings.Split(parts[0], ' ')[1]; commandType {
-
-    fmt.Println(parts)
-    userId := strings.Trim(parts[1], " ")
+    commandType := strings.Split(parts[0], " ")[1]
+    
+    switch commandType{
+    
+    case "BUY":
+      a64, _ := strconv.ParseInt(strings.Replace(parts[3],".","",-1),10,0)
+      var amount = int(a64)
+      stockSymbol := parts[2]
+      
+      buyCommand := BuyCommand{userId: userId, stockSymbol: stockSymbol, amount: amount}
+      userMap[userId] = append(userMap[userId], buyCommand)
+      // fmt.Println(buyCommand)
+    }
   }
 
-  //fmt.Println(userMap)
+  // fmt.Println(userMap)
 }
