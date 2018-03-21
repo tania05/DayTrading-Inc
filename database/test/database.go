f971bab50c56121feecb407f6d5667b4f94511ff
Brandon Mabey <brandonmabey@gmail.com>
2018-03-20 23:31:01 -0700

    More work on the database, still need transactions to work

--- database/test/database.go
+++ database/test/database.go
@@ -19,7 +19,7 @@ func waitForConnection() *sql.DB {
 		pingErr := db.Ping()
 		if pingErr != nil {
 			fmt.Println(pingErr)
-			fmt.Println("Retrying in 5s")
+			fmt.Println("Retrying in 1s")
 			time.Sleep(time.Second * 1)
 			continue
 		}
@@ -32,46 +32,74 @@ func waitForConnection() *sql.DB {
 	return db
 }
 
-/*
-	db.Query(`
+type queryable interface {
+	QueryRow(query string, args ...interface{}) *sql.Row
+	Exec(query string, args ...interface{}) (sql.Result, error)
+}
+
+func moneyReceive(db queryable, id string, money int) (int, error) {
+	row := db.QueryRow(`
 		INSERT INTO users(id,money)
 			VALUES ($1, $2)
 			ON CONFLICT(id) DO UPDATE
 				SET money = users.money + $2
-			RETURNING id,money;
-	`, "abcdef", 400)
+			RETURNING money;
+	`, id, money)
 
-	row, err := db.Query(`
+	var newBalance int
+	err := row.Scan(&newBalance)
+	return newBalance, err
+}
+
+func stockReceive(db queryable, userId string, stockSym string, amount int) (int, error) {
+	row := db.QueryRow(`
 		INSERT INTO stocks(user_id, stock_sym, amount)
 			VALUES ($1, $2, $3)
 			ON CONFLICT(user_id, stock_sym) DO UPDATE
 				SET amount = stocks.amount + $3
-			RETURNING user_id, stock_sym, amount;
-	`, "abcdef", "ABC", 120)
+			RETURNING amount;
+	`, userId, stockSym, amount)
 
-	row, err := db.Query(`
+	var newAmount int
+	err := row.Scan(&newAmount)
+	return newAmount, err
+}
+
+func moneyPay(db queryable, id string, money int) (int, error) {
+	row := db.QueryRow(`
 		UPDATE users
 			SET money = money - $2
 			WHERE id = $1
-			RETURNING id,money;
-	`, "abcdef", 250)
+			RETURNING money;
+	`, id, money)
 
-	row, err := db.Query(`
+	var newBalance int
+	err := row.Scan(&newBalance)
+	return newBalance, err
+
+}
+
+func stockPay(db queryable, userId string, stockSym string, amount int) (int, error) {
+	row := db.QueryRow(`
 		UPDATE stocks
 			SET amount = amount - $3
 			WHERE user_id = $1
 				AND stock_sym = $2
-			RETURNING user_id,stock_sym,amount;
-	`, "abcdef", "ABC", 200)
-*/
+			RETURNING amount;
+	`, userId, stockSym, amount)
+
+	var newAmount int
+	err := row.Scan(&newAmount)
+	return newAmount, err
+}
 
 func main() {
 	db := waitForConnection()
 
-
+	v, err := stockReceive(db, "user_b", "DEF", 200)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(row)
+	fmt.Printf("v %d\n", v)
 }
