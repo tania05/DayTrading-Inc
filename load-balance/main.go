8a9614a393f55dace40f6cd0515eef3ac13be7fc
Tal Melamed <tal.melamed.69@gmail.com>
2018-03-25 22:18:22 -0700

    distributed system

--- load-balance/main.go
+++ load-balance/main.go
@@ -6,9 +6,18 @@ import (
   "github.com/gorilla/mux"
   "log"
   "fmt"
+  "io/ioutil"
+  "encoding/json"
+  "strconv"
 )
 
-var ipMap []string = []string{"128.333.3.3:9090"}
+type RegisterServerCommand struct {
+	IP string
+	Port int
+}
+
+var ipMap []string = []string{}
+var rr int = 0
 
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
@@ -18,26 +27,42 @@ func HelloHandler(w http.ResponseWriter, r *http.Request) {
 func GetServerHandler(w http.ResponseWriter, r *http.Request) {
 	//body, _ := ioutil.ReadAll(r.Body)
 	//err := json.Unmarshal(body, &payload)
-
-	ip := ipMap[0];
-
-	fmt.Println("sent back")
-	fmt.Println(ipMap[0]);
 	defer r.Body.Close()
-	w.Header().Set("Content-Type", "text/plain")
-	w.Write([]byte(ip))
+	if (len(ipMap) < 1){
+		fmt.Println("no servers registered")
+		w.WriteHeader(http.StatusServiceUnavailable)
+	} else {
+		ip := ipMap[rr];
+		rr = (rr + 1) % len(ipMap)
+
+		fmt.Println("sent back")
+		fmt.Println(ip);
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(ip))
+	}	
 }
 
 func RegisterIPHandler(w http.ResponseWriter, r *http.Request) {
-	//body, _ := ioutil.ReadAll(r.Body)
+	var payload RegisterServerCommand
+	body, _ := ioutil.ReadAll(r.Body)
+	err := json.Unmarshal(body, &payload)
+	if err != nil {
+		panic(err)
+	}
 	defer r.Body.Close()
+	link := payload.IP + ":" + strconv.Itoa(payload.Port)
+	ipMap = append(ipMap, link)
+	w.WriteHeader(http.StatusOK)
+	fmt.Println("server registered: " + link)
+	fmt.Println(ipMap)
 }
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", HelloHandler)
 	r.Path("/users").Methods("GET").HandlerFunc(GetServerHandler);
-	r.Path("/register/{ip}/{port}").Methods("POST").HandlerFunc(RegisterIPHandler);
+	r.Path("/register").Methods("POST").HandlerFunc(RegisterIPHandler);
 
 	http.Handle("/", r)
 	addr := ":5555"
